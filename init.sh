2a003323a7f56b0f4eaef509a3501f2d02f530e7
Sequømics <sequomics@users.noreply.github.com>
2016-01-20 16:45:52 +0530

    Initial commit
    init.sh, shell script to --/initialize/-- whole "Sequømics®" in one go.
    Usage: $ sh init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+# ============================ Sequømics® ============================
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
