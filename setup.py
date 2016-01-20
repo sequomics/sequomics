e515bf6937fc6a45d04d43a0ca1f0e88ff3133d5
Sequømics <sequomics@users.noreply.github.com>
2016-01-21 02:36:45 +0530

    Initial commit
    Authors of Sequømics®.

--- setup.py
+++ setup.py
@@ -5,3 +5,7 @@ __company__ 	= 'Sequømics Corporation'
 __homepage__ 	= 'http://sequomics.com/'
 __account__ 	= 'sequomics'
 __githubURL__ 	= 'https://github.com/sequomics'
+__authors__ 	= [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
