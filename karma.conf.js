3e17072df58f6bf9cfacd1b000c3492ac97e647c
Sequømics <sequomics@users.noreply.github.com>
2016-01-21 01:38:12 +0530

    Initial commit
    --/The Heart of Test System/-- of "sequomics®".

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Sequomics® [v1.0.0]
+ * ______________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 20-Jan-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "Sequomics®".
+ * ______________________________________________________________
+ */
