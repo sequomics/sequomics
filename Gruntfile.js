98860881b3d78eca328a2156832e4c8a4d970226
Sequømics <sequomics@users.noreply.github.com>
2016-01-21 01:19:00 +0530

    Initial commit
    Help: http://gruntjs.com/api/grunt.task and http://gruntjs.com/configuring-tasks
    Usage: $ grunt task-name

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,3 +45,5 @@
 // ====================================
 // Grunt Task Registration through API.
 // ====================================
+// @method    : grunt.loadNpmTasks(pluginName)
+// @method    : grunt.registerTask(task-name)
