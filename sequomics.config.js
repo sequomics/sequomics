17f3a0866678aba167ace7bd467e23f73f817e65
Sequømics <sequomics@users.noreply.github.com>
2016-01-20 20:18:15 +0530

    Initial commit
    Configurations for Build/Test/Release [BTR] of Sequømics®, http://sequomics.com/.

---
+++ sequomics.config.js
@@ -0,0 +1,12 @@
+/*!
+ * sequomics.com® [v1.0.0]
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 20-Jan-2016
+ * @license   : Apache, version 2.0
+ * ______________________________________________________________
+ */
