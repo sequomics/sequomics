c71a8e3df9ce3129b5881a3fe8cee672631df554
Sequømics <sequomics@users.noreply.github.com>
2016-01-20 23:16:22 +0530

    Initial commit
    --/The Heart of Build System/-- of "sequomics®".

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Sequomics® [v1.0.0]
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 20-Jan-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "Sequomics®".
+ * ______________________________________________________________
+ */
