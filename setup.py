62757a0ec6488d7e628916124b1295adcf627c32
Sequømics <sequomics@users.noreply.github.com>
2016-01-21 02:40:14 +0530

    Initial commit
    License Type: Apache for Sequømics®.

--- setup.py
+++ setup.py
@@ -9,3 +9,22 @@ __authors__ 	= [
     '"Prabhat Kumar" <prabhat.genome@gmail.com>',
     '"Sequømics Corporation" <admin@sequomics.com>'
     ]
+__license__     = 'Apache License'
+
+# ------------------------------------------------------------------------
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0, (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       	http://www.apache.org/licenses/LICENSE-2.0
+#                           	or
+#   https://github.com/sequomics/sequomics/blob/master/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
